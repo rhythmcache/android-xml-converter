082475fee1a222e1ef41d8dc6aaa2a2a11bd0839
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-02-05 15:48:29 +0530

    Update abx2xml.cpp

--- abx2xml.cpp
+++ abx2xml.cpp
@@ -1,3 +1,39 @@
+/*
+Copyright 2021-2024, CCL Forensics
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+/*
+While this C++ implementation originates from:
+
+"https://github.com/rhythmcache/android-xml-converter"
+
+most of the inspiration and core logic are adapted from:
+
+"https://github.com/cclgroupltd/android-bits/blob/main/ccl_abx/ccl_abx.py"
+
+Due to this, I am including the original license text above to comply with the
+original licensing terms.
+*/
+
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,8 +46,6 @@
 #include <unordered_map>
 #include <algorithm>
 #include <cstring>
-
-// Based on https://github.com/cclgroupltd/android-bits/blob/main/ccl_abx/ccl_abx.py
 
 std::string base64_encode(const unsigned char* data, size_t len) {
     static const char base64_chars[] =
