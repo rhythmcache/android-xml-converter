35c1c1a043062829cd213e2d129489f1ef9cf056
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-02-07 01:43:49 +0530

    Update xml2abx.cpp

--- xml2abx.cpp
+++ xml2abx.cpp
@@ -1,10 +1,17 @@
-/*
- * This code is NOT LICENSED. You may use, modify, and distribute it freely. 
- * However, if you modify it, you MUST include this exact text at the top of the file. 
- * There is no warranty, and if it breaks, you get to keep both pieces. 
- * Attribution is appreciated but not required. 
- * Breaking this rule will not have legal consequences, but it will make me sad. :(
- */
+/*Copyright [2025] [rhythmcache]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 
 /*
